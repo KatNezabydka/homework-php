62f9722e208e08f705b7e03892a8a9e73b3ef569
Kat <katorif@ukr.net>
2018-05-12 12:08:11 +0300

    Task3 done

--- src/Task3/MarketHtmlPresenter.php
+++ src/Task3/MarketHtmlPresenter.php
@@ -1,16 +1,17 @@
 <?php declare(strict_types=1);
 
 namespace Cryptocurrency\Task3;
-
 use Cryptocurrency\Task1\CoinMarket;
-
 class MarketHtmlPresenter
 {
-    private $presentation = [];
     public function present(CoinMarket $market): string
     {
-        $this->presentation[] = $market->getCurrencies();
-        $this->presentation[] =  $market->maxPrice();
-        return serialize($this->presentation);
+        $html = '<div>';
+        foreach ($market->getCurrencies() as $currency) {
+            $html .= '<h1><img src="' . $currency->getLogoUrl() . '">'.$currency->getName().': '.$currency->getDailyPrice().'</h1>';
+        }
+        $html .= '</div>';
+        
+        return $html;
     }
 }
--- src/Task3/index.php
+++ src/Task3/index.php
@@ -1,22 +1,16 @@
 <?php
 
+
 require __DIR__ . '/../../vendor/autoload.php';
-
 use Cryptocurrency\Task1\CoinMarket;
-use Cryptocurrency\Task1\Bitcoin;
-use Cryptocurrency\Task1\Dogecoin;
-use Cryptocurrency\Task1\Ethereum;
+use Cryptocurrency\Task1\{Bitcoin,Ethereum,Dogecoin};
 use Cryptocurrency\Task3\MarketHtmlPresenter;
-
-// Fill in your market with currencies and use your presenter to show data here:
 $market = new CoinMarket();
 $market->addCurrency(new Bitcoin(700));
 $market->addCurrency(new Ethereum(800));
 $market->addCurrency(new Dogecoin(900));
 $marketPresenter = new MarketHtmlPresenter();
-$presentation = unserialize($marketPresenter->present($market));
-
-
+$presentation = $marketPresenter->present($market);
 ?>
 
 <!doctype html>
@@ -29,10 +23,6 @@ $presentation = unserialize($marketPresenter->present($market));
     <title>Built-in Web Server</title>
 </head>
 <body>
-<?php foreach ($presentation[0] as $present): ?>
- <h1><img src="<?php echo $present->getLogoUrl() ?>" alt=""><?php echo $present->getName() ?></h1>
-    <p>Price <?php echo $present->getName()?> today - <?php echo $present->getDailyPrice() ?></p>
-<?php endforeach; ?>
-<h2>Max price today is - <?php echo $presentation[1]?></h2>
+<?php echo $presentation ?>
 </body>
 </html>
