e33cd1970d8f67e62bac1bbfbec4cb67672d051e
Kat <katorif@ukr.net>
2018-05-12 12:10:44 +0300

    Task2 little change

--- src/Task2/EmojiGenerator.php
+++ src/Task2/EmojiGenerator.php
@@ -4,7 +4,13 @@ namespace Cryptocurrency\Task2;
 
 class EmojiGenerator
 {
-    private $emoji = ['🚀', '🚃', '🚄', '🚅', '🚇'];
+    private $emoji = [
+        "\u{1f680}",
+        "\u{1f683}",
+        "\u{1f684}",
+        "\u{1f685}",
+        "\u{1f687}"
+    ];
     public function generate(): \Generator
     {
        foreach ($this->emoji as $item){
